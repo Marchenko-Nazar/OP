0e6c2d0eae0a6117bbafb956addd4ff7fffb56b1
Home <Home@HOME>
2024-12-08 13:50:29 +0100

    C:\Users\arekh\source\repos\OP\Term1\Lab_1\Lab_1\Lab_1.csproj

---
+++ Term1/Lab_1/Lab_1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int sizeOfInt = sizeof(int);
+        int sizeOfShort = sizeof(short);
+        int sizeOfLong = sizeof(long);
+        int sizeOfChar = sizeof(char);
+        int sizeOfFloat = sizeof(float);
+        int sizeOfDouble = sizeof(double);
+
+        Console.WriteLine($"int: {sizeOfInt} bytes, short: {sizeOfShort} bytes, long: {sizeOfLong} bytes, char: {sizeOfChar} bytes, float: {sizeOfFloat} bytes, double: {sizeOfDouble} bytes");
+    }
+}
