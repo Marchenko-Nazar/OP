f8cb93b69733d0ea308bf2d8d3bfa08b2ade93a9
Home <Home@HOME>
2024-12-11 20:50:13 +0100

    C:\Users\arekh\Source\Repos\OP\Term1\Lab_1\Lab_1\Lab_1.csproj

--- Term1/Lab4/Lab4/Program.cs
+++ Term1/Lab4/Lab4/Program.cs
@@ -4,39 +4,36 @@ class Program
 {
     static void Main()
     {
-        // Комп'ютер загадує випадкове число від 1 до 100
         Random random = new Random();
-        int secretNumber = random.Next(1, 101);  // Генеруємо випадкове число від 1 до 100
-        int attempts = 0;  // Лічильник спроб
-        int low = 1;       // Нижня межа для бінарного пошуку
-        int high = 100;    // Верхня межа для бінарного пошуку
-        int guess;         // Число, яке програма припускає
+        int secretNumber = random.Next(1, 101);
+        int attempts = 0; 
+        int low = 1;     
+        int high = 100;    
+        int guess;         
 
         Console.WriteLine("Комп'ютер загадав число від 1 до 100. Спробуйте вгадати його!");
 
         while (true)
         {
-            // Здогадка як середнє значення між low та high
             guess = (low + high) / 2;
-            attempts++; // Збільшуємо кількість спроб
+            attempts++; 
 
             Console.WriteLine($"Спроба #{attempts}: Ваша здогадка - {guess}");
 
             if (guess < secretNumber)
             {
-                Console.WriteLine("Більше"); // Якщо здогадка менша за загадане число
-                low = guess + 1; // Зміщуємо нижню межу
+                Console.WriteLine("Більше"); 
+                low = guess + 1; 
             }
             else if (guess > secretNumber)
             {
-                Console.WriteLine("Менше"); // Якщо здогадка більша за загадане число
-                high = guess - 1; // Зміщуємо верхню межу
+                Console.WriteLine("Менше");
+                high = guess - 1; 
             }
             else
             {
-                // Якщо вгадали
                 Console.WriteLine($"Вітаємо! Ви вгадали число {secretNumber} за {attempts} спроб.");
-                break; // Вихід з циклу
+                break; 
             }
         }
     }
--- Term1/Lab_7/Lab_7/Program.cs
+++ Term1/Lab_7/Lab_7/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Приклад квадратної матриці
+        int[,] matrix =
+        {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+        };
+
+        // Вивід вихідної матриці
+        Console.WriteLine("Original Matrix:");
+        PrintMatrix(matrix);
+
+        int columnCount = ModifyMatrix(matrix);
+
+        // Вивід зміненої матриці
+        Console.WriteLine("\nModified Matrix:");
+        PrintMatrix(matrix);
+
+        // Вивід кількості стовпців
+        Console.WriteLine($"\nNumber of columns: {columnCount}");
+    }
+
+    static int ModifyMatrix(int[,] matrix)
+    {
+        int size = matrix.GetLength(0); 
+
+        for (int col = 0; col < size; col++)
+        {
+            int diagonalValue = matrix[size - col - 1, col];
+
+            for (int row = 0; row < size; row++)
+            {
+                matrix[row, col] *= diagonalValue; 
+            }
+        }
+
+        return size; 
+    }
+
+    static void PrintMatrix(int[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
